19ee8bf2d6635ecdd20b2b954f3fa956143791c8
bipin <bipinbipin@gmail.com>
2017-09-18 17:17:02 -0500

    Initial commit

---
+++ src/main/java/com/astontech/iot/api/HelloRest.java
@@ -0,0 +1,13 @@
+package com.astontech.iot.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloRest {
+
+    @GetMapping("/api/hi")
+    public String helloWorld() {
+        return "H3!!0 W()R!D";
+    }
+}
