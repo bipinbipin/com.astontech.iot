d2aa4c47a63d6b2773265aeacc97fd074c6db38d
bipin <bipinbipin@gmail.com>
2017-09-20 17:31:39 -0500

    CI test 4

--- src/main/java/com/astontech/iot/api/HelloRest.java
+++ src/main/java/com/astontech/iot/api/HelloRest.java
@@ -1,13 +1,27 @@
 package com.astontech.iot.api;
 
+import com.pi4j.io.gpio.*;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloRest {
 
+    public static GpioPinDigitalOutput pin;
+
     @GetMapping("/api/hi")
     public String helloWorld() {
         return "H3!!0 W()R!D     -   003";
+    }
+
+    @PostMapping("/api/led/")
+    public String toggleLed() {
+        if(pin==null) {
+            GpioController gpio = GpioFactory.getInstance();
+            pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_16, "LED 01", PinState.LOW);
+        }
+        pin.toggle();
+        return "Light Toggled!";
     }
 }
